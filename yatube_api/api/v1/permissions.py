da6b398c0e577b55d62982ac4869b3e06e508ea4
Даниэль Жариков <dan.zharikow2017@yandex.ru>
2023-03-17 17:24:53 +0200

    Sprint 9. Final project. V2

--- yatube_api/api/v1/permissions.py
+++ yatube_api/api/v1/permissions.py
@@ -5,8 +5,10 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     """
     The request is authenticated as a author, or is a read-only request.
     """
+    message = {'errors': ['You ate not the author of this content.']}
 
     def has_object_permission(self, request, view, obj):
+        self.message['errors'].clear()
         return (
             request.method in permissions.SAFE_METHODS
             or obj.author == request.user
---
+++ yatube_api/api/v1/urls.py
@@ -0,0 +1,22 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from api.v1.views import (CommentViewSet, FollowViewSet, GroupViewSet,
+                          PostViewSet)
+
+router = DefaultRouter()
+
+router.register(r'posts', PostViewSet, basename='posts')
+router.register(r'groups', GroupViewSet, basename='groups')
+router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
+router.register(r'follow', FollowViewSet, basename='follow')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', include('djoser.urls')),
+    path('', include('djoser.urls.jwt')),
+]
